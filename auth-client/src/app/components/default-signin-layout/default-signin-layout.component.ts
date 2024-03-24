7c31a16761841e9f5f284daffef78b44946653b3
Miguel Remédios <miguelremediios@gmail.com>
2024-03-24 17:26:16 +0000

    feat: scss styling for default sign-in component

--- auth-client/src/app/components/default-signin-layout/default-signin-layout.component.ts
+++ auth-client/src/app/components/default-signin-layout/default-signin-layout.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-default-signin-layout',
   standalone: true,
   imports: [],
   templateUrl: './default-signin-layout.component.html',
-  styleUrl: './default-signin-layout.component.scss'
+  styleUrl: './default-signin-layout.component.scss',
 })
 export class DefaultSigninLayoutComponent {
-
+  @Input() title: string = '';
+  @Input() primaryBtnText: string = '';
+  @Input() secondaryBtnText: string = '';
 }
