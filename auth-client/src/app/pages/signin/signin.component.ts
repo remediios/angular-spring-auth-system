83d2b8eaa23173bbbf594279a6d4b1c34b757a5b
Miguel Remédios <miguelremediios@gmail.com>
2024-03-24 18:11:24 +0000

    feat: sign in form component and relative adjustments for child components

--- auth-client/src/app/pages/signin/signin.component.ts
+++ auth-client/src/app/pages/signin/signin.component.ts
@@ -1,11 +1,33 @@
 import { Component } from '@angular/core';
 import { DefaultSigninLayoutComponent } from '../../components/default-signin-layout/default-signin-layout.component';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { PrimaryInputComponent } from '../../components/primary-input/primary-input.component';
 
 @Component({
   selector: 'app-signin',
   standalone: true,
-  imports: [DefaultSigninLayoutComponent],
+  imports: [
+    DefaultSigninLayoutComponent,
+    ReactiveFormsModule,
+    PrimaryInputComponent,
+  ],
   templateUrl: './signin.component.html',
   styleUrl: './signin.component.scss',
 })
-export class SigninComponent {}
+export class SigninComponent {
+  signInForm!: FormGroup;
+  constructor() {
+    this.signInForm = new FormGroup({
+      username: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(6),
+      ]),
+    });
+  }
+}
