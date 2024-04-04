d0a3f3f30393e4e37482eaf9973e19e4d0c362fb
Miguel Remédios <miguelremediios@gmail.com>
2024-04-04 20:03:32 +0100

    feat: SecurityFilter for Spring Security layer

---
+++ auth-server/src/main/java/com/example/authserver/infra/security/SecurityFilter.java
@@ -0,0 +1,43 @@
+package com.example.authserver.infra.security;
+import com.example.authserver.domain.user.User;
+import com.example.authserver.repositories.UserRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Collections;
+@Component
+public class SecurityFilter extends OncePerRequestFilter  {
+    @Autowired
+    TokenService tokenService;
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        var token = this.recoverToken(request);
+        var loginEmail = tokenService.validateToken(token);
+
+        if(loginEmail != null){
+            User user = userRepository.findByEmail(loginEmail).orElseThrow(() -> new RuntimeException("User Not Found"));
+            var authorities = Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER"));
+            var authentication = new UsernamePasswordAuthenticationToken(user, null, authorities);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private String recoverToken(HttpServletRequest request){
+        var authHeader = request.getHeader("Authorization");
+        if(authHeader == null) return null;
+        return authHeader.replace("Bearer ", "");
+    }
+}
--- auth-server/src/main/java/com/example/authserver/repositories/UserRepository.java
+++ auth-server/src/main/java/com/example/authserver/repositories/UserRepository.java
@@ -3,6 +3,8 @@ package com.example.authserver.repositories;
 import com.example.authserver.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, String> {
+import java.util.Optional;
 
+public interface UserRepository extends JpaRepository<User, String> {
+    Optional<User> findByEmail(String email);
 }
