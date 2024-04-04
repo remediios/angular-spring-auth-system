f8798c3929408850d31c849027a914c6c9cf8e8f
Miguel Remédios <miguelremediios@gmail.com>
2024-04-04 19:32:50 +0100

    feat: TokenService validateToken and generateExpiration functions

--- auth-server/src/main/java/com/example/authserver/infra/security/TokenService.java
+++ auth-server/src/main/java/com/example/authserver/infra/security/TokenService.java
@@ -1,10 +1,16 @@
 package com.example.authserver.infra.security;
 
+import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.example.authserver.domain.user.User;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 
 @Service
 public class TokenService {
@@ -13,9 +19,31 @@ public class TokenService {
     public String generateToken(User user){
         try{
             Algorithm algorithm = Algorithm.HMAC256(secret);
-//            String token = algorithm.
+             String token = JWT.create()
+                     .withIssuer("auth-server")
+                     .withSubject(user.getEmail())
+                     .withExpiresAt(this.generateExpirationDate())
+                     .sign(algorithm);
+             return token;
         } catch(JWTCreationException exception) {
             throw new RuntimeException("Error while authenticating");
         }
+    }
+
+    public String validateToken(String token){
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    .withIssuer("auth-server")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+        } catch (JWTVerificationException exception){
+            return null;
+        }
+    }
+
+    private Instant generateExpirationDate(){
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("+01:00"));
     }
 }
